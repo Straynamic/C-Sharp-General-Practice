7360bbbb5440fefd88bac0c7d21d0044e4a04758
Jesse <straynamic@gmail.com>
2020-09-24 17:49:21 -0700

    Added Question3

--- c# Lessons/Task3/Program.cs
+++ c# Lessons/Task3/Program.cs
@@ -7,10 +7,11 @@ namespace Task3
         static void Main(string[] args)
         {
             Console.WriteLine("Started Program");
-            Question2.RunQuestion();
+            Question3.RunQuestion();
         }
     }
 }
 
 //Question 1: What is Boxing and Unboxing in C#?
 //Question 2: What is enum in C#?
+//Question 3: What is the difference between constant and readonly in C#?
---
+++ c# Lessons/Task3/Question3.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace Task3
+{
+    public static class Question3
+    {
+        //Question 3: What is the difference between constant and readonly in C#?
+        /*
+            Const is nothing but "constant", a variable of which the value is constant but at compile time. It's mandatory to assign a value to it.
+            By default, a const is static and we cannot change the value of a const variable throughout the entire program.
+            Readonly is the keyword whose value we can change during runtime or we can assign it at run time but only through the non-static constructor.
+        */
+        public static void RunQuestion(){
+            Test obj = new Test();    
+            obj.Check();    
+            //Console.ReadLine(); 
+        }
+    }
+
+    class Test {    
+        readonly int read = 10;    
+        const int cons = 10;    
+        public Test() {    
+            read = 100;    
+        }    
+        public void Check() {    
+            Console.WriteLine("Read only : {0}", read);    
+            Console.WriteLine("const : {0}", cons);    
+        }  
+    }
+}
+
